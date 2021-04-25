5300b5aadce6ca292812a8f41973220190735b1c
Ali Moghanni <a.moghanni>
2021-04-25 11:49:52 +0200

    create numpy.py file

---
+++ numpy.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  networkx
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-24
+ * ============================================================================
+ """
+
+# Python package for creating and manipulating complex networks
+
+# Link : https://anaconda.org/anaconda/networkx
+
+# To install this package with conda run:
+
+conda install - c anaconda networkx
