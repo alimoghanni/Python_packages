9fa7949cca2206a9883ef2c9b5d64acea2e70583
Ali Moghanni <a.moghanni>
2021-04-26 11:27:35 +0200

    Add Matplotlib commit

---
+++ matplotlib.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  numpy
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-25
+ * ============================================================================
+ """
+
+# Array processing for numbers, strings, records, and objects
+
+# Link : https://anaconda.org/anaconda/numpy
+
+# To install this package with conda run:
+
+conda install - c anaconda numpy
