c31ed5e474f43ebec41dcdbc61371ba391ef8912
Ali Moghanni <a.moghanni>
2021-04-27 09:51:22 +0200

    add pandas

---
+++ pandas.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  pandas
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-27
+ * ============================================================================
+ """
+
+# High-performance, easy-to-use data structures and data analysis tools
+
+# Link : https://anaconda.org/anaconda/pandas
+
+# To install this package with conda run:
+
+conda install -c anaconda pandas
