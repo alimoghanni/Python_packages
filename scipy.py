8a710a727e203cc31251820af80dfcfbe433de7a
Ali Moghanni <a.moghanni>
2021-04-28 09:29:52 +0200

    add scipy.py

---
+++ scipy.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  scipy
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-28
+ * ============================================================================
+ """
+
+# Scientific Library for Python
+
+# Link : https://anaconda.org/anaconda/scipy
+
+# To install this package with conda run:
+
+conda install -c anaconda scipy
