7c9be0b2a9dd200bf799031d980065dea1c1e6e6
Ali Moghanni <a.moghanni>
2021-04-28 13:47:28 +0200

    add torchvision

---
+++ torchvision.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  torchvision
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-29
+ * ============================================================================
+ """
+
+# Image and video datasets and models for torch deep learning
+
+# Link : https://anaconda.org/anaconda/torchvision
+
+# To install this package with conda run:
+
+conda install -c anaconda torchvision
