dee746138a7d40b3e06699dff59bc78b6da06776
Ali Moghanni <a.moghanni>
2021-04-29 15:20:37 +0200

    Add pytorch and scikit-learn

---
+++ pytorch.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  pytorch
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-29
+ * ============================================================================
+ """
+
+# PyTorch is an optimized tensor library for deep learning using GPUs and CPUs
+
+# Link : https://anaconda.org/pytorch/pytorch
+
+# To install this package with conda run:
+
+conda install -c pytorch pytorch
---
+++ scikit-learn.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  scikit-learn
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-29
+ * ============================================================================
+ """
+
+# A set of python modules for machine learning and data mining
+
+# Link : https://anaconda.org/anaconda/scikit-learn
+
+# To install this package with conda run:
+
+conda install -c anaconda scikit-learn
