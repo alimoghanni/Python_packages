2f56cc24f4877969aaa0db9c801b19b63d1c8bea
Ali Moghanni <a.moghanni>
2021-04-30 11:35:38 +0200

    add rstudio

---
+++ r-essentials.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  r-essentials
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-30
+ * ============================================================================
+ """
+
+# Some essential packages for working with R
+
+# Link : https://anaconda.org/r/r-essentials
+
+# To install this package with conda run:
+
+conda install -c r r-essentials
---
+++ rstudio.py
@@ -0,0 +1,21 @@
+"""
+ * ============================================================================
+ * Python Packages  --  rstudio
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-04-30
+ * ============================================================================
+ """
+
+# A set of integrated tools designed to help you be more productive with R
+
+# Link : https://anaconda.org/r/rstudio
+
+# To install this package with conda run one of the following:
+
+conda install -c r rstudio
+conda install -c r/label/borked rstudio
