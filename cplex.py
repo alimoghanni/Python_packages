3a67ecd695312bdb1dd003f3ab7104afd72c9466
Ali Moghanni <a.moghanni>
2021-05-02 13:27:02 +0200

    add cplex

---
+++ cplex.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  cplex
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-05-02
+ * ============================================================================
+ """
+
+# The IBM CPLEX Community Edition runtime for Python
+
+# Link : https://anaconda.org/ibmdecisionoptimization/cplex
+
+# To install this package with conda run:
+
+conda install - c ibmdecisionoptimization cplex
