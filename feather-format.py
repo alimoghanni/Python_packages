3d439af5e918c4a821d018f397332d9401fe4fd1
Ali Moghanni <a.moghanni>
2021-05-04 13:22:58 +0200

    add feather-format

---
+++ feather-format.py
@@ -0,0 +1,21 @@
+"""
+ * ============================================================================
+ * Python Packages  --  feather-format
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-05-04
+ * ============================================================================
+ """
+
+# Feather: fast, interoperable binary data frame storage for Python, R, and more powered by Apache Arrow
+
+# Link : https://anaconda.org/conda-forge/feather-format
+
+# To install this package with conda run one of the following:
+
+conda install - c conda-forge feather-format
+conda install - c conda-forge/label/cf201901 feather-format
