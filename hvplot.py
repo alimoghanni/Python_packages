5eb2422ae2682f5f3da915ef4f7c62c4acffdcef
Ali Moghanni <a.moghanni>
2021-05-06 10:47:56 +0200

    add hvplot

---
+++ hvplot.py
@@ -0,0 +1,21 @@
+"""
+ * ============================================================================
+ * Python Packages  --  hvplot
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-05-06
+ * ============================================================================
+ """
+
+# A high-level plotting API for the PyData ecosystem built on HoloViews.
+
+# Link : https://anaconda.org/pyviz/hvplot
+
+# To install this package with conda run one of the following:
+
+conda install - c pyviz hvplot
+conda install - c pyviz/label/dev hvplot
