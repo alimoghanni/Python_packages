0bd4af25a73abaec7d7a0923b9781529e47a65f5
Ali Moghanni <a.moghanni>
2021-05-21 11:43:48 +0200

    add jupyterlab

---
+++ jupyterlab.py
@@ -0,0 +1,23 @@
+"""
+ * ============================================================================
+ * Python Packages  --  jupyterlab
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-05-21
+ * ============================================================================
+ """
+
+# An extensible environment for interactive and reproducible computing, based on the Jupyter Notebook and Architecture.
+
+# Link : https://anaconda.org/conda-forge/jupyterlab
+
+# To install this package with conda run one of the following:
+
+conda install -c conda-forge jupyterlab
+conda install -c conda-forge/label/broken jupyterlab
+conda install -c conda-forge/label/prerelease-jupyterlab jupyterlab
+conda install -c conda-forge/label/cf201901 jupyterlab
