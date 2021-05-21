aa799ecb53f073c69b22813bcf8c99905203e97b
Ali Moghanni <a.moghanni>
2021-05-21 11:48:16 +0200

    add psycopg2

---
+++ psycopg2.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  psycopg2
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-05-21
+ * ============================================================================
+ """
+
+# PostgreSQL database adapter for Python
+
+# Link : https://anaconda.org/anaconda/psycopg2
+
+# To install this package with conda run:
+
+conda install - c anaconda psycopg2
