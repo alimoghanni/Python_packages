ca6a9b719632a08c909aaf7da6ce49cb7fa492ab
Ali Moghanni <a.moghanni>
2021-07-02 11:16:23 +0200

    add seaborn

---
+++ seaborn.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  seaborn
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-07-02
+ * ============================================================================
+ """
+
+# Statistical data visualization
+
+# Link : Link: https://anaconda.org/anaconda/seaborn
+
+# To install this package with conda run:
+
+conda install - c anaconda seaborn
