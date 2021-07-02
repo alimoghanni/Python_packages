2d19d8b622c70b9ac609533d3b8406f74616db55
Ali Moghanni <a.moghanni>
2021-07-02 11:01:55 +0200

    add sympy and tikzplotlib

---
+++ sympy.py
@@ -0,0 +1,20 @@
+"""
+ * ============================================================================
+ * Python Packages  --  sympy
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-07-02
+ * ============================================================================
+ """
+
+# Python library for symbolic mathematics
+
+# Link : https://anaconda.org/anaconda/sympy
+
+# To install this package with conda run:
+
+conda install -c anaconda sympy
---
+++ tikzplotlib.py
@@ -0,0 +1,21 @@
+"""
+ * ============================================================================
+ * Python Packages  --  tikzplotlib
+ * 
+ * @author Ali Moghanni
+ * Resources:
+ *
+ * This .py file can be obtained at https://github.com/alimoghanni/Python_packages.
+ *
+ * updated: 2021-06-13
+ * ============================================================================
+ """
+
+# Convert matplotlib figures into TikZ/PGFPlots
+
+# Link : https://anaconda.org/conda-forge/tikzplotlib
+
+# To install this package with conda run one of the following:
+
+conda install -c conda-forge tikzplotlib
+conda install -c conda-forge/label/cf202003 tikzplotlib
